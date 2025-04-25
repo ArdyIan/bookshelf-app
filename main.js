227ed5d7327015bafaee841a0e08325aeba7a5a8
Ardy1an <adithardiyanto0@gmail.com>
2025-04-25 17:07:56 +0700

    adding id value

--- main.js
+++ main.js
@@ -51,19 +51,22 @@ function addBook() {
 
 function createBookElement(book) {
   const title = document.createElement("h3");
-  title.className = "text-lg font-bold text-gray-800 ";
+  title.className = "text-lg font-bold text-gray-800";
   title.innerText = book.title;
+  title.setAttribute("data-testid", "bookItemTitle");
 
   const author = document.createElement("p");
   author.innerText = `Penulis: ${book.author}`;
+  author.setAttribute("data-testid", "bookItemAuthor");
 
   const year = document.createElement("p");
   year.innerText = `Tahun: ${book.year}`;
+  year.setAttribute("data-testid", "bookItemYear");
 
   const toggleButton = document.createElement("button");
   toggleButton.innerText = book.isComplete ? "Pindah ke belum selesai dibaca" : "Pindah ke selesai dibaca";
-
   toggleButton.className = "bg-blue-500 text-white p-1 rounded cursor-pointer hover:bg-blue-700";
+  toggleButton.setAttribute("data-testid", "bookItemIsCompleteButton");
 
   toggleButton.addEventListener("click", () => {
     book.isComplete = !book.isComplete;
@@ -73,8 +76,8 @@ function createBookElement(book) {
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "Hapus buku";
-
   deleteButton.className = "bg-blue-500 text-white p-1 rounded cursor-pointer hover:bg-blue-700 ml-2";
+  deleteButton.setAttribute("data-testid", "bookItemDeleteButton");
 
   deleteButton.addEventListener("click", () => {
     books = books.filter((b) => b.id !== book.id);
@@ -82,17 +85,22 @@ function createBookElement(book) {
     renderBooks();
   });
 
+  const buttonContainer = document.createElement("div");
+  buttonContainer.append(toggleButton, deleteButton);
+
   const container = document.createElement("div");
   container.classList.add("book-item");
   container.className = "bg-white shadow-md rounded-lg p-4 mb-4 border-1 border-gray-300";
-  container.append(title, author, year, toggleButton, deleteButton);
+  container.setAttribute("data-bookid", book.id);
+  container.setAttribute("data-testid", "bookItem");
+  container.append(title, author, year, buttonContainer);
 
   return container;
 }
 
 function renderBooks(filteredBooks = null) {
-  const incompleteShelf = document.getElementById("incompleteBookshelfList");
-  const completeShelf = document.getElementById("completeBookshelfList");
+  const incompleteShelf = document.getElementById("incompleteBookList");
+  const completeShelf = document.getElementById("completeBookList");
 
   incompleteShelf.innerHTML = "";
   completeShelf.innerHTML = "";
